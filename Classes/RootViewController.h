bf3f215d34ca83d30e620801383e3a5c82cb2545
erguolinge <erguolinge@gmail.com>
2011-02-13 12:43:34 +0900

    Navi+CoreData初期作成
    SQLデバッグオプション追加

---
+++ Classes/RootViewController.h
@@ -0,0 +1,22 @@
+//
+//  RootViewController.h
+//  MigDemoAuto
+//
+//  Created by 蒼い惑星 on 11/02/13.
+//  Copyright 2011 蒼い惑星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+@private
+    NSFetchedResultsController *fetchedResultsController_;
+    NSManagedObjectContext *managedObjectContext_;
+}
+
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+
+@end
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MigDemoAuto
+//
+//  Created by 蒼い惑星 on 11/02/13.
+//  Copyright 2011 ボイジャー. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
